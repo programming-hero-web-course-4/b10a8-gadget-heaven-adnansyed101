7fe2843c28bf6716867b9e946ed924796e882013
adnan <sirgeant10@gmail.com>
2024-11-04 22:01:36 +0600

    Add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import ProductDetail from "./components/ProductDetail.jsx";
+import Banner from "./components/Banner.jsx";
+import Gadgets from "./components/Gadgets.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: (
+          <div>
+            <Banner></Banner>
+            <Gadgets></Gadgets>
+          </div>
+        ),
+      },
+      {
+        path: "details/:id",
+        element: <ProductDetail></ProductDetail>,
+        loader: () => fetch("/tech.json"),
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
