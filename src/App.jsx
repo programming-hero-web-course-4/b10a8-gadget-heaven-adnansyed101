d55a2e9206b2d23d75a2d0458763258cbd311328
adnan <sirgeant10@gmail.com>
2024-11-04 22:06:14 +0600

    Use app as context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import Banner from "./components/Banner";
+import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
-import Gadgets from "./components/Gadgets";
 import Navbar from "./components/Navbar";
 
 const App = () => {
+  const [techs, setTechs] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All Products");
+
+  useEffect(() => {
+    fetch("/tech.json")
+      .then((res) => res.json())
+      .then((data) => {
+        if (selectedCategory === "All Products") {
+          setTechs(data);
+        } else {
+          setTechs(data.filter((tech) => tech.category === selectedCategory));
+        }
+      });
+  }, [selectedCategory]);
+
+  const handleSelectedCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <>
       <Navbar></Navbar>
-      <Banner></Banner>
-      <Gadgets></Gadgets>
+      <Outlet
+        context={{ techs, selectedCategory, handleSelectedCategory }}
+      ></Outlet>
       <Footer></Footer>
     </>
   );
