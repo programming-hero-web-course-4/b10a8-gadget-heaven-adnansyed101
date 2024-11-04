612320b3ecb4e3b8c5cd5716720ab4b19c225944
adnan <sirgeant10@gmail.com>
2024-11-04 22:04:18 +0600

    Remove relative and absolute

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,23 +2,20 @@ import bannerImg from "../assets/banner.jpg";
 
 const Banner = () => {
   return (
-    <div className="hero bg-primary container mx-auto rounded-b-lg text-white relative pb-52 mb-60">
-      <div className="hero-content text-center">
-        <div className="max-w-5xl">
-          <h1 className="text-5xl font-bold">
-            Upgrade Your Tech Accessorize with Gadget Heaven Accessories
-          </h1>
-          <p className="py-6">
-            Explore the latest gadgets that will take your experience to the
-            next level. From smart devices to the coolest accessories, we have
-            it all!
-          </p>
-          <button className="btn bg-white text-primary rounded-full">
-            Shop Now
-          </button>
-        </div>
+    <div className="flex flex-col items-center container mx-auto text-white mb-10 text-center">
+      <div className="bg-primary pt-10 pb-36 rounded-b-lg">
+        <h1 className="text-5xl font-bold">
+          Upgrade Your Tech Accessorize with Gadget Heaven Accessories
+        </h1>
+        <p className="py-6">
+          Explore the latest gadgets that will take your experience to the next
+          level. From smart devices to the coolest accessories, we have it all!
+        </p>
+        <button className="btn bg-white text-primary rounded-full">
+          Shop Now
+        </button>
       </div>
-      <figure className="absolute top-64 w-2/4 outline outline-offset-8 rounded-lg">
+      <figure className="w-2/4 outline outline-offset-8 rounded-lg -mt-32">
         <img src={bannerImg} alt="Apple Vision" className="rounded-lg" />
       </figure>
     </div>
