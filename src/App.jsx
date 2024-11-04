17eb102c0ebd4f95fbadc604882b8a47b3b8da43
adnan <sirgeant10@gmail.com>
2024-11-04 13:18:23 +0600

    Show Gadgets section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Banner from "./components/Banner";
+import Gadgets from "./components/Gadgets";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Gadgets></Gadgets>
     </>
   );
 };
