6c5bcdcaa25e48b4f1fdaff467575b6ac0674d46
adnan <sirgeant10@gmail.com>
2024-11-05 21:48:37 +0600

    Create Stats comp

---
+++ src/components/Stats.jsx
@@ -0,0 +1,10 @@
+const Stats = () => {
+  return (
+    <div className="bg-primary py-10 w-full text-white text-center mb-10">
+      <h1 className="text-5xl font-bold mb-10">Statistics</h1>
+      <h2>No Data to Show</h2>
+    </div>
+  );
+};
+
+export default Stats;
