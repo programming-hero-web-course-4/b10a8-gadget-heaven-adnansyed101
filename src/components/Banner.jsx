40cfcd6f283146dc8da3eefaed71186d2ce12073
adnan <sirgeant10@gmail.com>
2024-11-05 19:09:56 +0600

    Add width to banner text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,11 +3,11 @@ import bannerImg from "../assets/banner.jpg";
 const Banner = () => {
   return (
     <div className="flex flex-col items-center container mx-auto text-white mb-10 text-center">
-      <div className="bg-primary pt-10 pb-48 rounded-b-lg">
-        <h1 className="text-5xl font-bold">
+      <div className="bg-primary pt-10 pb-52 rounded-b-lg w-full flex flex-col items-center">
+        <h1 className="text-5xl font-bold w-2/3">
           Upgrade Your Tech Accessorize with Gadget Heaven Accessories
         </h1>
-        <p className="py-6">
+        <p className="py-6 w-2/3">
           Explore the latest gadgets that will take your experience to the next
           level. From smart devices to the coolest accessories, we have it all!
         </p>
