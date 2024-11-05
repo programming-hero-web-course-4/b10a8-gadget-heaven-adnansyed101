8442dbd3b6ff634f25e4efd5173373b4c2850d74
adnan <sirgeant10@gmail.com>
2024-11-05 19:40:57 +0600

    Create blogs

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import BlogsCard from "./BlogsCard";
+
+const Blogs = () => {
+  const blogs = useLoaderData();
+
+  return (
+    <section className="mb-10">
+      <div className="bg-primary py-10 w-full text-white text-center mb-10">
+        <h1 className="text-5xl font-bold mb-10">Blogs</h1>
+      </div>
+      <div className="container mx-auto grid grid-cols-3 gap-4">
+        {blogs.map((blog) => (
+          <BlogsCard key={blog.id} blog={blog}></BlogsCard>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Blogs;
