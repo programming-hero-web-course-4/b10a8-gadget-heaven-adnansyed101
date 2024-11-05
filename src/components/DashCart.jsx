3da973e573f6e013ec58e44a8ee92caf0627fb5f
adnan <sirgeant10@gmail.com>
2024-11-05 18:59:38 +0600

    Sort techs using price

--- src/components/DashCart.jsx
+++ src/components/DashCart.jsx
@@ -3,17 +3,27 @@ import { GiSettingsKnobs } from "react-icons/gi";
 import { useOutletContext } from "react-router-dom";
 
 const DashCart = () => {
-  const { selectedTechs } = useOutletContext();
+  const { selectedTechs, handleSort } = useOutletContext();
+
+  const totalAmount = selectedTechs.reduce(
+    (total, tech) => total + tech.price,
+    0
+  );
 
   return (
     <>
       <div className="container mx-auto flex items-center gap-4 mb-10">
         <h4 className="flex-1 font-bold text-xl">Cart</h4>
-        <p className="font-bold text-xl">Total Cost: $999</p>
-        <button className="btn btn-outline rounded-full border-primary text-primary">
+        <p className="font-bold text-xl">Total Cost: ${totalAmount}</p>
+        <button
+          className="btn btn-outline rounded-full border-primary text-primary"
+          onClick={handleSort}
+        >
           Sort By Price <GiSettingsKnobs />
         </button>
-        <button className="btn btn-outline rounded-full bg-primary text-white font-semibold text-xl">
+        <button
+          className="btn btn-outline rounded-full bg-primary text-white font-semibold text-xl"
+        >
           Purchase
         </button>
       </div>
