b248946047af57c1bbeaa08ff936fd5f49485493
adnan <sirgeant10@gmail.com>
2024-11-05 15:40:20 +0600

    Create Dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import DashCart from "./DashCart";
+import DashWish from "./DashWish";
+
+const Dashboard = () => {
+  const [tab, setSelectedTab] = useState("cart");
+
+  const handleSelectedTab = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <section className="mb-10">
+      <div className="bg-primary py-10 w-full text-white text-center mb-10 ">
+        <h1 className="text-5xl font-bold">Dashboard</h1>
+        <p className="py-6">
+          Explore the latest gadgets that will take your experience to the next
+          level. From smart devices to the coolest accessories, we have it all!
+        </p>
+        <div className="space-x-2 w-1/4 mx-auto flex">
+          <button
+            className={`btn rounded-full w-1/2 ${
+              tab === "cart"
+                ? "bg-white text-primary"
+                : "border-white text-white bg-primary"
+            }`}
+            onClick={() => handleSelectedTab("cart")}
+          >
+            Cart
+          </button>
+          <button
+            className={`btn rounded-full w-1/2 ${
+              tab === "wishlist"
+                ? "bg-white text-primary"
+                : "border-white text-white bg-primary"
+            }`}
+            onClick={() => handleSelectedTab("wishlist")}
+          >
+            Wishlist
+          </button>
+        </div>
+      </div>
+      {tab === "cart" ? <DashCart></DashCart> : <DashWish></DashWish>}
+    </section>
+  );
+};
+
+export default Dashboard;
