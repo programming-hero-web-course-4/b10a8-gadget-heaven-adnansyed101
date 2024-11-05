c10906d46fed1772c8d77093d92cd4626b085add
adnan <sirgeant10@gmail.com>
2024-11-05 19:10:19 +0600

    Remove banner text

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,12 +7,8 @@ const Dashboard = () => {
 
   return (
     <section className="mb-10">
-      <div className="bg-primary py-10 w-full text-white text-center mb-10 ">
-        <h1 className="text-5xl font-bold">Dashboard</h1>
-        <p className="py-6">
-          Explore the latest gadgets that will take your experience to the next
-          level. From smart devices to the coolest accessories, we have it all!
-        </p>
+      <div className="bg-primary py-10 w-full text-white text-center mb-10">
+        <h1 className="text-5xl font-bold mb-10">Dashboard</h1>
         <div className="space-x-2 w-1/4 mx-auto flex">
           <button
             className={`btn rounded-full w-1/2 ${
